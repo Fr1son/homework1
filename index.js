f77ddb5b471fa4bd0beb4dec4e03472945babd82
Fr1son <ihor.malenkov18@gmail.com>
2023-10-03 16:59:35 +0300

    end

---
+++ index.js
@@ -0,0 +1,4 @@
+alert("Hello");
+let fisrtName = prompt("What is your name?");
+let country = prompt("Where are you from?");
+alert('Nice to meet you ' + fisrtName + '! ' + 'You are from ' + country);
